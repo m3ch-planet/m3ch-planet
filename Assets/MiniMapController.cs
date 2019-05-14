e5fda71c5129112f2780c738fdd8e6477c507469
Huy Ha <hqh2101@columbia.edu>
2019-05-13 22:30:56 -0400

    minimap camera follows player

---
+++ Assets/MiniMapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    GameController GC;
+    PlayerController LocalPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        LocalPlayer = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LocalPlayer == null && GC.GetGameHappening())
+        {
+            LocalPlayer = GC.GetLocalPlayer();
+        }else if (GC.GetGameHappening())
+        {
+            transform.position = LocalPlayer.transform.position + LocalPlayer.transform.up * 1.45f;
+            transform.LookAt(LocalPlayer.transform);
+        }
+    }
+}
