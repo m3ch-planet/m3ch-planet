4880d85e14212801e9c892cc343d1c5e3bcb0f16
Matthew Chan <matthew.a.chan@gmail.com>
2019-05-14 06:37:00 -0400

    Fix power up spawning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -248,7 +248,7 @@ public class PlayerController : NetworkBehaviour
         GameObject Planet = Instantiate(AM.Get("Planet"));
         NetworkServer.Spawn(Planet);
         Planet.gameObject.name = "Planet " + Planet.GetComponent<NetworkIdentity>().netId.ToString();
-        TC.CmdInitPowerUps(Planet);
+        //Planet.GetComponent<Planet>().seed = (int) Time.time;
         RpcStartGame(Planet);
     }   
 
